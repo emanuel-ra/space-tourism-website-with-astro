59e18f43487f16a441b92da9e1be326a51cd143d
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2024-01-29 12:08:59 -0600

    🖊 Finish destination page

--- app/src/components/Destination/index.tsx
+++ app/src/components/Destination/index.tsx
@@ -1,19 +1,93 @@
 import { useState } from "react";
-
-type TPlanet = "MOON" | "MARS" | "EUROPA" | "TITAN";
+import { planets, type Planets } from "../../lib/planets";
 
 export const Destination = () => {
-  const [planet, setPlanet] = useState<TPlanet>("MOON");
+  const [planet, setPlanet] = useState<Planets>(planets[0]);
+
+  const handlePlane = (planet: Planets) => {
+    setPlanet(planet);
+  };
+
   return (
     <>
-      <h1> 01 Pick your destination</h1>
-      <section></section>
+      <section className="flex max-lg:gap-y-4 flex-col max-lg:items-center lg:flex-row lg:justify-around">
+        <Planet image={planet.image} title={planet.title} />
+        <Information planets={planets} handler={handlePlane} planet={planet} />
+      </section>
     </>
   );
 };
 
-const Planet = () => {
-  return <h1>hELLO</h1>;
+const Planet = (props: { image: string; title: string }) => {
+  const { image, title } = props;
+  return (
+    <div className="animate-fade-right animate-ease-in-out transition rounded-full w-1/2 lg:w-1/3">
+      <img
+        src={`./images/destination/${image}`}
+        alt={title}
+        className="animate-brightness shadow-[0_0px_100px_10px_rgba(255,255,255,0.5)] rounded-full"
+      />
+    </div>
+  );
 };
 
-const Information = () => {};
+const Information = ({
+  planets,
+  handler,
+  planet,
+}: {
+  planets: Planets[];
+  handler: (planet: Planets) => void;
+  planet: Planets;
+}) => {
+  return (
+    <>
+      <article className="lg:w-1/3 flex flex-col max-md:items-center max-lg:gap-2 lg:gap-4 uppercase animate-fade-left animate-ease-in-out transition">
+        <nav>
+          <ul className="flex gap-2 uppercase text-white/70  ">
+            {planets.map((option) => (
+              <li
+                className={`hover:text-white hover:border-b-2 hover:border-white transition
+                ${
+                  option.title === planet.title
+                    ? "text-white border-b-2 border-white transition"
+                    : ""
+                }
+                `}
+              >
+                <a
+                  href={`#`}
+                  onClick={() => {
+                    handler(option);
+                  }}
+                >
+                  {option.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+
+        <h2 className="max-lg:text-4xl max-lg:text-center lg:text-7xl uppercase">
+          {planet.title}
+        </h2>
+        <blockquote className="text-pretty max-lg:text-center">
+          {planet.description}
+        </blockquote>
+        <hr className="bg-white/50 text-white/50 max-md:my-4 lg:my-10" />
+        <div className="flex max-md:flex-col max-md:items-center max-md:gap-y-3 justify-between">
+          <article className="flex flex-col">
+            <span className="uppercase text-white/70">Avg. distance</span>
+            <span className="uppercase text-white text-2xl">
+              {planet.distance}
+            </span>
+          </article>
+          <article className="flex flex-col">
+            <span className="uppercase text-white/70">Est. travel time</span>
+            <span className="uppercase text-white text-2xl">{planet.time}</span>
+          </article>
+        </div>
+      </article>
+    </>
+  );
+};
--- app/tailwind.config.mjs
+++ app/tailwind.config.mjs
@@ -1,13 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       backgroundColor: {
-        primary: '#0B0D17',
-        secondary: '#D0D6F9'
-      }
-    }
+        primary: "#0B0D17",
+        secondary: "#D0D6F9",
+      },
+      animation: (theme) => ({
+        brightness: "brightness 1.5s infinite;",
+        "spin-slow": "spin 3s linear infinite",
+      }),
+      keyframes: {
+        brightness: {
+          "0%, 100%": { filter: "brightness(1);" },
+          "50%": { filter: "brightness(1.50);" },
+        },
+      },
+    },
   },
-  plugins: [require('tailwindcss-animated')]
-}
+  plugins: [require("tailwindcss-animated")],
+};
